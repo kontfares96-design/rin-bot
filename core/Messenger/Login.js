39622abde87230ce4e07e194c9509f14aead5962
kontfares96-design <kontfares96@gmail.com>
2026-07-22 17:52:30 +0100

    Update Login.js

--- core/Messenger/Login.js
+++ core/Messenger/Login.js
@@ -9,11 +9,8 @@ const Logger = require("../../utils/logger");
 class Login {
 
 	constructor(config) {
-
 		this.config = config;
-
 		this.api = null;
-
 		this.connected = false;
 
 		this.statePath = path.join(
@@ -21,47 +18,34 @@ class Login {
 			"config",
 			"rinstate.json"
 		);
-
 	}
 
 	loadState() {
-
 		if (!fs.existsSync(this.statePath)) {
-
-			throw new Error(
-				"لم يتم العثور على rinstate.json"
-			);
-
+			throw new Error("لم يتم العثور على rinstate.json");
 		}
 
 		return JSON.parse(
 			fs.readFileSync(this.statePath, "utf8")
 		);
-
 	}
 
 	saveState(appState) {
-
 		fs.writeFileSync(
 			this.statePath,
 			JSON.stringify(appState, null, 2)
 		);
-
 	}
 
 	getAPI() {
-
 		return this.api;
-
 	}
 
 	isConnected() {
-
 		return this.connected;
-
 	}
 
-}	async connect() {
+	async connect() {
 
 		Logger.system("جاري تسجيل الدخول إلى Facebook...");
 
@@ -82,64 +66,48 @@ class Login {
 				(err, api) => {
 
 					if (err) {
-
 						Logger.error(err.message);
-
 						return reject(err);
-
 					}
 
 					this.api = api;
-
 					this.connected = true;
 
 					try {
-
 						const newState = api.getAppState();
-
 						this.saveState(newState);
-
-					}
-					catch {}
+					} catch {}
 
 					Logger.success("تم تسجيل الدخول بنجاح.");
 
 					resolve(api);
-
 				}
 			);
 
 		});
+	}
 
-}
 	async disconnect() {
 
 		if (!this.api)
 			return;
 
 		try {
-
 			if (typeof this.api.logout === "function")
 				await this.api.logout();
-
-		}
-		catch {}
+		} catch {}
 
 		this.api = null;
-
 		this.connected = false;
 
 		Logger.warning("تم تسجيل الخروج.");
-
 	}
 
 	async reconnect() {
-
 		await this.disconnect();
-
 		return this.connect();
-
 	}
 
 }
+
 module.exports = Login;
