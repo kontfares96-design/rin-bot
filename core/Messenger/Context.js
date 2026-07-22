1a55cae7e9fd954d4ca6256271457129ab9c34b6
kontfares96-design <kontfares96@gmail.com>
2026-07-22 16:57:20 +0100

    Update Context.js

--- core/Messenger/Context.js
+++ core/Messenger/Context.js
@@ -1,1 +1,120 @@
+"use strict";
 
+class Context {
+
+	constructor(data = {}) {
+
+		this.api = data.api || null;
+
+		this.event = data.event || null;
+
+		this.body = data.body || "";
+
+		this.command = data.command || null;
+
+		this.args = data.args || [];
+
+		this.prefix = data.prefix || "/";
+
+		this.threadID = data.threadID || null;
+
+		this.senderID = data.senderID || null;
+
+		this.messageID = data.messageID || null;
+
+		this.mentions = data.mentions || {};
+
+		this.attachments = data.attachments || [];
+
+		this.timestamp = data.timestamp || Date.now();
+
+	}
+
+	async reply(body) {
+
+		return new Promise((resolve, reject) => {
+
+			this.api.sendMessage(
+				{ body },
+				this.threadID,
+				(err, info) => {
+
+					if (err)
+						return reject(err);
+
+					resolve(info);
+
+				},
+				this.messageID
+			);
+
+		});
+
+	}
+
+	async send(body) {
+
+		return new Promise((resolve, reject) => {
+
+			this.api.sendMessage(
+				{ body },
+				this.threadID,
+				(err, info) => {
+
+					if (err)
+						return reject(err);
+
+					resolve(info);
+
+				}
+			);
+
+		});
+
+	}
+
+	async react(emoji) {
+
+		return new Promise((resolve, reject) => {
+
+			this.api.setMessageReaction(
+				emoji,
+				this.messageID,
+				err => {
+
+					if (err)
+						return reject(err);
+
+					resolve(true);
+
+				},
+				true
+			);
+
+		});
+
+	}
+
+	async unsend() {
+
+		return new Promise((resolve, reject) => {
+
+			this.api.unsendMessage(
+				this.messageID,
+				err => {
+
+					if (err)
+						return reject(err);
+
+					resolve(true);
+
+				}
+			);
+
+		});
+
+	}
+
+}
+
+module.exports = Context;
