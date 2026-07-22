acbdb33f622130f2f4953caa5802cf88d1320552
kontfares96-design <kontfares96@gmail.com>
2026-07-22 17:28:57 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,1 +1,27 @@
+"use strict";
 
+module.exports = {
+	name: "message",
+
+	description: "Message Event",
+
+	async execute({ api, event, router }) {
+
+		try {
+
+			if (!event) return;
+
+			if (!event.body && !event.attachments) return;
+
+			await router.handle(event);
+
+		}
+		catch (err) {
+
+			console.error("[MESSAGE EVENT]", err);
+
+		}
+
+	}
+
+};
